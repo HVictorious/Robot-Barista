813af2d683cb294383392de15c887bb5b79f66a8
HVictorious <108153042+HVictorious@users.noreply.github.com>
2022-06-24 15:55:40 -0400

    Add files via upload

---
+++ Barista_3.0.py
@@ -0,0 +1,73 @@
+#Robot Barista
+#@Author:Hunter Victorious
+
+print("Hello, welcome to the Cafe!!!!!!")
+
+name = input("What is your name?\n")
+
+#Verifies that you are not Evil Ben 
+if name == "Ben":
+    evil_status = input("Are you Evil?\n")
+    if evil_status == "Yes" or evil_status == "yes":
+        print("You're not welcome here Evil Ben!! Get out!!")
+        exit()
+    else:
+        print("Oh, so you're one of those good Bens. Come on in!!")
+else:
+    print("Hello " + name + ", thank you so much for coming in today.\n\n\n")
+
+#Input for different types of coffee
+menu_list = ["Black Coffee", "Espresso", "Latte", "Cappucino", "Frappuccino", "Dark Roast"]
+
+while True:
+    order = input(name + ",what would you like from our menu today? Here is what we are serving.\n" + ('\n'.join(map(str, menu_list))) + "\n")
+    try:
+        if order in menu_list:
+            break
+        else:
+            print("Please Try again!")
+    except:
+        continue
+
+if order == "Frappuccino":
+    price_coffee = 5
+elif order == "Black Coffee":
+    price_coffee = 2
+elif order == "Espresso":
+    price_coffee = 6
+elif order == "Cuppucino":
+    price_coffee = 4
+elif order == "Latte":
+    price_coffee = 7
+elif order == "Dark Roast":
+    price_coffee = 3
+
+#Input for different price and cost difference by price
+size_list = ["Small", "Medium", "Large"]
+
+while True:
+    size = input(name + ",what would you like from our menu today? Here is what we are serving.\n" + ('\n'.join(map(str, size_list))) + "\n")
+    try:
+        if size in size_list:
+            break
+        else:
+            print("Please Try again!")
+    except:
+        continue
+
+if size == "Small":  
+    price = price_coffee * int(1)
+elif size == "Medium":
+    price = price_coffee * int(2)
+elif size == "Large": 
+    price = price_coffee * int(3)
+
+#Enables customer to be able to buy more than one item
+quantity = input("How many coffees would you like?\n")
+
+#Calculates all inputs and gives your total price
+total = price * int(quantity) 
+
+print("Thank you. Your total is: $" + str(total))
+
+print("Sounds good " + name + ", we'll have that out for you in just one moment")
